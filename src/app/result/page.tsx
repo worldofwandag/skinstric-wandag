b26a68db28f5910136050cfd48dca3271473cc67
worldofwandag <jwandag@gmail.com>
2025-04-25 00:51:01 -0700

    more ui detail fixes for result page

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -12,30 +12,30 @@ import BackBtn from "../components/ui/BackBtn";
 
 const Page = () => {
   return (
-    <div className="min-h-[90vh] flex flex-col bg-white relative md:pt-[64px]">
+    <div className="min-h-[90vh] flex flex-col bg-white relative md:pt-[64px] justify-center">
       <div className="absolute top-2 left-9 md:left-8 text-left">
         <p className="font-semibold text-xs md:text-sm">TO START ANALYSIS</p>
       </div>
-      <div className="flex-1 flex flex-col md:flex-row items-center xl:justify-center relative mb-20 md:mb-60 space-y-16 md:space-y-0">
+      <div className="flex-1 flex flex-col md:flex-row items-center xl:justify-center relative mb-0 md:mb-20 space-y-16 md:space-y-0">
         {/* LEFT SIDE */}
         <div className="relative md:absolute md:left-[55%] lg:left-[50%] xl:left-[40%] md:-translate-y-[0%] -translate-y-[-20%] md:-translate-x-full flex flex-col items-center cursor-pointer justify-center">
           {/* for anchoring */}
-          <div className="w-[380px] h-[380px] md:w-[482px] md:h-[482px]" />
+          <div className="w-[270px] h-[270px] md:w-[482px] md:h-[482px]" />
 
           <Image
             src={ResDiamondLarge}
             alt="Diamond Large"
-            className="absolute w-[380px] h-[380px] md:w-[482px] md:h-[482px] animate-spin-slow"
+            className="absolute w-[270px] h-[270px] md:w-[482px] md:h-[482px] animate-spin-slow"
           />
           <Image
             src={ResDiamondMedium}
             alt="DiamondMedium"
-            className="absolute w-[340px] h-[340px] md:w-[444.34px] md:h-[444.34px] animate-spin-slower"
+            className="absolute w-[230px] h-[230px] md:w-[444.34px] md:h-[444.34px] animate-spin-slower"
           />
           <Image
             src={ResDiamondSmall}
             alt="DiamondSmall"
-            className="absolute w-[300px] h-[300px] md:w-[405.18px] md:h-[405.18px] animate-spin-slowest"
+            className="absolute w-[190px] h-[190px] md:w-[405.18px] md:h-[405.18px] animate-spin-slowest"
           />
 
           {/* Camera Icon and Text */}
@@ -43,12 +43,12 @@ const Page = () => {
             <Image
               src={cameraIcon}
               alt="Camera Icon"
-              className="absolute"
+              className="absolute w-[100px] h-[100px] md:w-[136px] md:h-[136px]"
               width={136}
               height={136}
             />
 
-            <div className="absolute bottom-[9%] right-[90px]  md:top-[30.9%] md:right-[-12px] translate-y-[-20px]">
+            <div className="absolute bottom-[1%] right-[90px]  md:top-[30.9%] md:right-[-12px] translate-y-[-20px]">
               <p className="text-xs md:text-sm font-normal mt-1 leading-[24px]">
                 ALLOW A.I.
                 <br />
@@ -67,28 +67,25 @@ const Page = () => {
 
         {/* RIGHT SIDE */}
         <div className="relative md:absolute md:left-[50%] lg:left-[55%] xl:left-[60%] flex flex-col items-center cursor-pointer mt-12 md:mt-0 justify-center md:-translate-y-[0%] -translate-y-[10%]">
-          <div className="w-[380px] h-[380px] md:w-[482px] md:h-[482px]" />
+          <div className="w-[270px] h-[270px] md:w-[482px] md:h-[482px]" />
 
           <Image
             src={ResDiamondLarge}
             alt="Diamond Large"
-            className="absolute w-[380px] h-[380px] md:w-[482px] md:h-[482px] animate-spin-slow"
-            width={380}
-            height={380}
+            className="absolute w-[270px] h-[270px] md:w-[482px] md:h-[482px] animate-spin-slow"
+            
           />
           <Image
             src={ResDiamondMedium}
             alt="DiamondMedium"
-            className="absolute w-[340px] h-[340px] md:w-[444.34px] md:h-[444.34px] animate-spin-slower"
-            width={340}
-            height={340}
+            className="absolute w-[230px] h-[230px] md:w-[444.34px] md:h-[444.34px] animate-spin-slower"
+            
           />
           <Image
             src={ResDiamondSmall}
             alt="DiamondSmall"
-            className="absolute w-[300px] h-[300px] md:w-[405.18px] md:h-[405.18px] animate-spin-slowest"
-            width={220}
-            height={220}
+            className="absolute w-[190px] h-[190px] md:w-[405.18px] md:h-[405.18px] animate-spin-slowest"
+            
           />
 
           {/* PHOTO GALLERY UPLOAD  */}
@@ -96,12 +93,12 @@ const Page = () => {
             <Image
               src={galleryIcon}
               alt="Photo Upload Icon"
-              className="absolute"
+              className="absolute w-[100px] h-[100px] md:w-[136px] md:h-[136px]"
               width={136}
               height={136}
             />
             <div className="absolute top-[75%] md:top-[70%]  md:left-[17px] translate-y-[-10px]">
-              <p className="text-[12px] md:text-[14px] font-normal mt-1 leading-[24px] text-right">
+              <p className="text-[12px] md:text-[14px] font-normal mt-2 leading-[24px] text-right">
                 ALLOW A.I.
                 <br />
                 ACCESS GALLERY
@@ -131,7 +128,7 @@ const Page = () => {
           <BackBtn />
         </Link>
 
-        {/* PROCESS BUTTON*/}
+        {/* SUMMARY BUTTON*/}
 
         {/* small screens */}
         <Link href="/result" className="md:hidden">
